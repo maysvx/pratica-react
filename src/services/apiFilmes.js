ae230cdde8bece447d887e752d4753cc44485afd
Mayara Silva Vieira <72236606+maysvx@users.noreply.github.com>
2022-04-14 17:44:05 -0300

    Update apiFilmes.js

--- src/services/apiFilmes.js
+++ src/services/apiFilmes.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiFilmes = axios.create({
     baseURL: 'https://api.themoviedb.org/3/',
     headers:{
-        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmQ3M2NjZmNmY2UwNzg0ODEwMGRmOWQ2OWQ4OTM4ZSIsInN1YiI6IjVhOTE1ZjM3OTI1MTQxNDE3ZDAwOTUzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1LQBovGOn69k7BpSS0BUeJZ1sFAvrSPUz7ydm7ekKS0',
+        'Authorization': '',
         'Content-type': 'application/json;charset=utf-8',
     }
 })
